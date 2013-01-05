24fd71ca234e136114855b1d9d41b82e2be782fa
Matthew Skolaut <tech2077@gmail.com>
2013-01-05 14:50:18 -0600

    Add a group for autonomous

---
+++ Infrared/src/commands/autonomousgroup/Auton.java
@@ -0,0 +1,36 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package commands.autonomousgroup;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+/**
+ *
+ * @author matthew
+ */
+public class Auton extends CommandGroup
+{
+    public Auton()
+    {
+        
+    }
+    
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    }
+    
+    // Called at end of Command
+    public void end(){
+    }
+    
+    // Call end() if interrupted
+    public void interrupted(){
+        end();
+    }
+}
