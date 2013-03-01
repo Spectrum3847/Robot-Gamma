3bbf439774428e07d6d97d1eeac6615b9528acc7
Matthew Skolaut <tech2077@gmail.com>
2013-03-01 16:37:24 -0600

    Fixed incorrect autonomous timing, smartdashboard exception still must be fixed

--- Infrared/src/commands/autonomousgroup/Auton.java
+++ Infrared/src/commands/autonomousgroup/Auton.java
@@ -7,7 +7,6 @@ import commands.shoot.AutonFire;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import edu.wpi.first.wpilibj.command.WaitCommand;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import framework.Init;
 
 /**
  *
@@ -18,20 +17,11 @@ public class Auton extends CommandGroup
     public Auton() {
         super();
         requires(CommandBase.drivebase);
-        addSequential(new AutonDriveCollect());
-        addSequential(new WaitCommand(SmartDashboard.getNumber("Auton Collect Drive Time")));
-        addSequential(new AutonLift());
-        addSequential(new WaitCommand(SmartDashboard.getNumber("Auton Lift Time")));
-        addSequential(new AutonFire());
-        addSequential(new WaitCommand(SmartDashboard.getNumber("Auton Fire Time")));
-        addSequential(new AutonFire());
-        addSequential(new WaitCommand(SmartDashboard.getNumber("Auton Fire Time")));
-        addSequential(new AutonFire());
-        addSequential(new WaitCommand(SmartDashboard.getNumber("Auton Fire Time")));
-        addSequential(new AutonFire());
-        addSequential(new WaitCommand(SmartDashboard.getNumber("Auton Fire Time")));
-        //addParallel(Init.lautodrive);
-        //addParallel(Init.rautodrive);
+        addSequential(new AutonDriveCollect(), SmartDashboard.getNumber("Auton Collect Drive Time"));
+        addSequential(new AutonLift(), SmartDashboard.getNumber("Auton Lift Time"));
+        addSequential(new AutonFire(), SmartDashboard.getNumber("Auton Fire Time"));
+        addSequential(new AutonFire(), SmartDashboard.getNumber("Auton Fire Time"));
+        addSequential(new AutonFire(), SmartDashboard.getNumber("Auton Fire Time"));
     }
     
     // Called just before this Command runs the first time
