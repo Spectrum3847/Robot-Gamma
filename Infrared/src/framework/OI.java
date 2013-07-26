7a490edde4864eda84ccc12f1722818f48aa69dc
Zachary  Meves <zmeves@spectrum3847.org>
2013-07-25 20:07:13 -0500

    Pool Noodle 5

--- Infrared/src/framework/OI.java
+++ Infrared/src/framework/OI.java
@@ -39,6 +39,7 @@ public class OI {
     public static final Button front_dec = new JoystickButton(gamepad_aux.getGamepad(), Gamepad.X_BUTTON);
     
     public static final Button brakes = new JoystickButton(gamepad.getGamepad(), Gamepad.LEFT_CLICK);
+    
     public static final Button hooks = new JoystickButton(gamepad.getGamepad(), Gamepad.RIGHT_BUMPER);
     
     //Use this constructor to setup up button schedulers for commands
